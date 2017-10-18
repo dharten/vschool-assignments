f400dd80f07975b4ed3130d94ff9e7be9a1b4006
Dan Harten <danharten@bigstyx-AIR.local>
2017-10-18 17:55:05 -0600

    loops, if's, newsies, star wars, objects-in-the-roomm

---
+++ exercises/if-statement-olympics/app.js
@@ -0,0 +1,52 @@
+// if (5 > 3) {
+//   console.log("is greater than");
+// }
+// // var cat = 3;
+//   if (cat.length = 3) {
+//     console.log("is the length");
+//   }
+// // if ("cat" === "dog") {
+//   console.log("try again");
+// } else {
+//   console.log("not the same");
+// }
+// var person = {
+//   name: "ted",
+//   age: 12
+// }
+// if (person.age >= 12) {
+//   console.log(person.name + " is allowed to go to the movie");
+// } else {
+//   console.log(person.name + " is NOT allowed to go to the movie");
+// }
+// var person = {
+//   name: "bed",
+//   age: 20
+// }
+// if (person.name[0] === "b" && person.age >= 18) {
+//   console.log(person.name + " is allowed to go to the movie");
+// } else {
+//   console.log(person.name + " is NOT allowed to go to the movie");
+// }
+// if (1 > "1") {
+//   console.log("strict");
+// } else if (1 === "1") {
+//   console.log("abstract");
+// } else {
+//   console.log("not equal at all");
+// }
+// if (1 <= 2 && 2 === 4 || (3 * 4) > 10 && (5 + 10 > 10)) {
+//   console.log("yes");
+// }
+// if (typeof "dog" === "string"){
+//   console.log("yes");
+// }
+var booleanValue
+  if (booleanValue === true) {
+    console.log("it is");
+  }
+  if (typeof booleanValue !== "undefined") {
+    console.log("it is defined");
+  } else {
+    console.log("you got it")
+  }
