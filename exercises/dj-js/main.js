f508776ee6d95cd7e8b6b6f706dea59b9412174b
Dan Harten <big.styx08@gmail.com>
2017-10-24 15:59:50 -0600

    dj-js

--- exercises/dj-js/main.js
+++ exercises/dj-js/main.js
@@ -1,5 +1,26 @@
-var red = document.getElementById("square").addEventListener('click', red);
+var square = document.getElementById('change')
 
-function changeToRed() {
-  red.style.color = document.addEventListener('click', red);
+function blue(){
+  this.style.backgroundColor = 'blue';
 }
+
+function red(){
+  this.style.backgroundColor = 'red';
+}
+
+function yellow(){
+  this.style.backgroundColor = 'yellow';
+}
+
+function green(){
+  this.style.backgroundColor = 'green';
+}
+
+square.addEventListener('mouseover', blue);
+square.addEventListener('mousedown', red);
+square.addEventListener('mouseup', yellow);
+square.addEventListener('dblclick', green);
+
+document.addEventListener('mousewheel', function() {
+  square.style.backgroundColor = 'orange';
+})
