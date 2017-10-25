12a0ea7b9b5910c7c0d7075e8fe1836b2b52483f
Dan Harten <danharten@bigstyx-AIR.local>
2017-10-25 11:49:22 -0600

    bootstrap-color-grid

--- exercises/random-password-gererator/main.js
+++ exercises/random-password-gererator/main.js
@@ -1,10 +1,10 @@
 function generatePassword(length) {
         characters = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*QWERTYUIOPASDFGHJKLZXCVBNM",
         returnedValue = "";
-    for (var i = 0, n = characters.length; i < length; i++) {
-        returnedValue += characters.charAt(Math.floor(Math.random() * n));
+    for (var i = 0; i < length; i++) {
+        returnedValue += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     console.log(returnedValue);
 }
 
-generatePassword(128);
+generatePassword(26);
