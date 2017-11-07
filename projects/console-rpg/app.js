235f41f3bc772ca3b8c8e632c9d29924bf1d3593
Dan Harten <big.styx08@gmail.com>
2017-11-07 13:39:15 -0700

    console-rpg

--- projects/console-rpg/app.js
+++ projects/console-rpg/app.js
@@ -1,33 +1,136 @@
+
 const ask = require('readline-sync');
 
 console.log('Welcome to Colossal RPG');
 
-const name = ask.question('What is your name?');
+const name = ask.question('What is your name? ');
 
-console.log(`Hopefully you don't regret playing ${name}. Lets get started!`);
+const playerHP = 50;
 
-const options = ['Walk', 'Check inventory', 'Take a nap'];
+const inventory = ['Stick', 'Hands', 'Dagger'];
 
-// const inventory = {
-//   weapon: squirt-gun,
-//   drink: water,
-//   clothing: sweater
-// }
-while (options !== 4) {
-  const pickOption = ask.keyInSelect(options, 'What would you like to do?');
+const player = {'name': name, 'hp': playerHP, 'inventory': inventory};
+
+let currentMonster;
+
+console.log(`\nHopefully you don't regret playing ${name}.
+You're starting with ${playerHP} HP. Start walking ${name}!`);
+
+const options = ['Walk', 'Pick a fight', 'Print'];
+
+
+function print() {
+  console.log('player:', player);
+  monsters.map(function(monster) {
+    if (monster.encountered){
+      console.log('monster: ', monster)
+    }
+  })
+}
+
+function choose() {
+  while (player.hp > 0) {
+  // let pickOption = ask.keyInSelect(options, 'What would you like to do?');
+  let pickOption = ask.keyIn("\nPress 'w' to walk or 'f' to fight ", {limit: "wfp"});
   switch (pickOption){
-    case 0: //walk
-      console.log('walk');//algorithm to determine if an enemy is found
+    case "w":
+      walk();
       break;
-    case 1: //check inventory
-      console.log('inventory');//open inventory
+    case "f": //check inventory
+      createMonster();
       break;
-    case 2: //Take a nap
-      console.log('Naptime')//3 second count down (...), add one HP, console.log('You added one HP!')
+    case "p":
+      print();
       break;
-    case 3: //CANCEL
-      //ask.keyInSelect('Are you sure?')
-      //true ? console.log(`You have left the game... chicken`): pickoption;
-      break;
+  };
+}}
+
+function walk(){
+  const  isThereMonster = Math.floor(Math.random() * 100);
+  // console.log('Is there a monster? ' + isThereMonster);
+  isThereMonster <= 33 ? createMonster(): console.log("Keep walkin");
+}
+
+const monsters = [
+  { monster: 'Wolf', hp: 15, encountered: false },
+  { monster: 'Bear', hp: 20, encountered: false },
+  { monster: 'Demon', hp: 35, encountered: false }]
+
+const fightOrFlight = ['Run', 'Fight'];
+
+function createMonster(){
+  currentMonster = Math.floor(Math.random() * 10) % 3;
+  monsters[currentMonster].encountered = true;
+  console.log('Generated Monster ' + currentMonster);
+  let pickOption = ask.keyInSelect(fightOrFlight,
+    `\nYou've encountered a ${monsters[currentMonster].monster} HP ${monsters[currentMonster].hp}
+What do you wanna do now?`);
+  if (pickOption === 0) {
+    run();
+  } else {
+    fight();
   }
 }
+
+function run(){
+  const getAway = Math.floor(Math.random() * 2);
+  if (getAway === 0) {
+    monsterAttack();
+  } else {
+    console.log('You got away!! Keep Walkin');
+  };
+}
+
+const fightOptions = ['Attack', 'Run'];
+
+function fight(){
+  while (monsters[currentMonster].hp > 0 && player.hp > 0) {
+    console.log(player.name, "HP", player.hp)
+    console.log(monsters[currentMonster].monster, "HP", monsters[currentMonster].hp);
+    let fightClub = ask.keyInSelect(fightOptions, "Do you want to attack or run?");
+    if (fightClub === 0) {
+      attackMonster();
+      monsterAttack();
+    } else if (fightClub === 1){
+      run();
+      if (run() === 1) {
+        monsterAttack();
+      } else {
+        break;
+      }
+    }
+  }
+  if (player.hp <= 0) {
+    die();
+  }
+  if (monsters[currentMonster].hp <= 0) {
+    enemyDie();
+  }
+}
+
+function attackMonster(){
+  const attackPower = Math.floor(Math.random() * 10)
+  monsters[currentMonster].hp -= attackPower;
+  // console.log(`Your attack has taken ${attackPower} HP from the ${monsters[currentMonster].monster}`)
+}
+
+function monsterAttack(){
+  let enemyAttackPower = Math.floor(Math.random() * 10);
+  player.hp -= enemyAttackPower;
+  console.log("You got away but not without getting hurt by the monster")
+//   console.log(`The ${monsters[currentMonster].monster} attacked you.
+// Your HP went down ${enemyAttackPower}.`)
+}
+
+function die(){
+ console.log("You're dead");
+}
+
+function enemyDie(){
+  player.hp += 5;
+  console.log(`You have killed the ${monsters[currentMonster].monster}!
+You earned 5 HP!`)
+}
+
+
+choose();
