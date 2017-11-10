1719851327c639d0ff003999f67d3757c178e8ea
Dan Harten <danharten@bigstyx-AIR.local>
2017-11-10 08:41:49 -0700

    homepage

---
+++ exercises/homepage/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import NavbarContent from "./NavbarContent";
+import Info from "./Info";
+import Services from "./Services";
+import Images from "./Images";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <NavbarContent />
+      <Info />
+      <Services />
+      <Images />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ exercises/homepage/src/Images.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Images() {
+  return (
+    <div>
+      <img src={ require("./images/cylinder-compression-test.jpg") } />
+    </div>
+  )
+}
+
+export default Images;
