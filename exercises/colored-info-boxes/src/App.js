6771cbd60c692a3a90d3c0537c147b4a013b94a3
Dan Harten <big.styx08@gmail.com>
2017-11-10 23:09:53 -0700

    colored-info-boxes, homepage, vacation-spots

---
+++ exercises/colored-info-boxes/src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Box from "./Box";
+
+function App() {
+  return (
+    <div>
+      <Box
+        title="Egg One"
+        subTitle="Uno"
+        description="For the money!!"
+        backgroundColor="#0000FF"
+      />
+      <Box
+        title="Egg Two"
+        subTitle="Dos"
+        description="For the show"
+        backgroundColor="#8A2BE2"
+      />
+      <Box
+        title="Egg Three"
+        subTitle="Tres"
+        description="To get ready!"
+        backgroundColor="#A52A2A"
+      />
+      <Box
+        title="Egg Four"
+        subTitle="Cuatro"
+        description="To go"
+        backgroundColor="#DEB887"
+      />
+      <Box
+        title="Egg Five"
+        subTitle="Cinco"
+        description="Ducks went out to play"
+        backgroundColor="#7FFF00"
+      />
+      <Box
+        title="Egg Six"
+        subTitle="Seis" 
+        description="Times a sinner"
+        backgroundColor="#008B8B"
+      />
+      <Box
+        title="Egg Seven"
+        subTitle="Siete"
+        description="A heavenly number"
+        backgroundColor="#A9A9A9"
+      />
+      <Box
+        title="Egg Eight"
+        subTitle="Ocho"
+        description="My favorite number"
+        backgroundColor="#556B2F"
+      />
+      <Box
+        title="Egg Nine"
+        subTitle="Nueve"
+        description="Evil towers"
+        backgroundColor="#FF00FF"
+      />
+      <Box
+        title="Egg Ten"
+        subTitle="Diez"
+        description="A multiple of 2 and 5"
+        backgroundColor="#008000"
+      />
+    </div>
+  )
+}
+
+export default App;
