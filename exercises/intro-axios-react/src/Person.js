60d31a957b9d78dc8b6ae734b73eb65d8c95221b
Dan Harten <danharten@bigstyx-AIR.local>
2017-11-17 08:44:18 -0700

    hit-list

---
+++ exercises/intro-axios-react/src/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Person(props) {
+  return <li>{ props.f }</li>
+}
+
+export default Person;
--- exercises/travel-form/src/UserInfo.js
+++ exercises/travel-form/src/UserInfo.js
@@ -11,22 +11,18 @@ class UserInfo extends Component {
       destination: "",
       dietaryRestrictions: [
         {
-          id: 1,
           label: "Vegetarian",
           checked: false
         },
         {
-          id: 2,
           label: "Lactose Intollerant",
           checked: false
         },
         {
-          id: 3,
           label: "Kosher",
           checked: false
         },
         {
-          id: 3,
           label: "Mamaz cooking",
           checked: false
         }
@@ -49,11 +45,11 @@ class UserInfo extends Component {
   handleSubmit (event) {
     event.preventDefault();
     alert(`First Name: ${ this.state.fname }
-      Last Name: ${ this.state.lname }
-      Age: ${ this.state.age }
-      Gender: ${ this.state.gender }
-      Destination: ${ this.state.destination }
-      Dietary Restrictions:${ this.state.dietaryRestrictions.map(restriction => {
+Last Name: ${ this.state.lname }
+Age: ${ this.state.age }
+Gender: ${ this.state.gender }
+Destination: ${ this.state.destination }
+Dietary Restrictions:${ this.state.dietaryRestrictions.map(restriction => {
         if (restriction.checked) {
           return " " + restriction.label
         } else {
@@ -61,97 +57,80 @@ class UserInfo extends Component {
         }
       })
     }`)
+    this.setState({ [event.target.name]: " " })
   }
-
-  this.state.dietaryRestrictions.map(restriction => {
-          if (restriction.checked) {
-            return " " + restriction.label
-          } else {
-            return null
-          }
-        })
-      }
-
 
   render() {
     return(
-        <form onSubmit={ this.handleSubmit }>
-            <div>
-              <label>
-                First Name:
-                <input name="fname"
-                  value={ this.state.fname }
-                  onChange={ this.handleUserData }
-                />
-                Last Name:
-                  <input name="lname"
-                    value={ this.state.lname }
-                    onChange={ this.handleUserData }
-                  />
-              </label>
-            </div>
-          <div>
-            <label>
-              Age:
-                <input name="age"
-                  value={ this.state.age }
-                  onChange={ this.handleUserData }
-                  type="number"
-                />
-            </label>
+      <form onSubmit={ this.handleSubmit }>
+        <div>
+          <label>
+            First Name:
+            <input name="fname" value={ this.state.fname }
+              onChange={ this.handleUserData }
+            />
+          </label>
+        </div>
+        <br></br>
+        <div>
+          <label>
+            Last Name:
+            <input name="lname" value={ this.state.lname }
+              onChange={ this.handleUserData }
+            />
+          </label>
+        </div>
+        <br></br>
+        <div>
+          <label>
+            Age:
+            <input name="age" value={ this.state.age }
+              onChange={ this.handleUserData } type="number" />
+          </label>
+        </div>
+        <br></br>
+        <div>
+          <label>
+            Gender:
+              Male
+              <input name="gender" value="male" type="radio"
+                onChange={ this.handleUserData }
+              />
+              Female
+              <input name="gender" value="female" type="radio"
+                onChange={ this.handleUserData }
+              />
+              Neither
+              <input name="gender" value="neither" type="radio"
+                onChange={ this.handleUserData }
+              />
+          </label>
+        </div>
+        <br></br>
+        <div>
+          <label>
+            Destination:
+            <select name="destination" onChange={ this.handleUserData }>
+              <option>{/*leave blank*/}</option>
+              <option value="Denver, CO">Denver, CO</option>
+              <option value="Jacksonville, FL">Jacksonville, FL</option>
+              <option value="Perth, AUS">Perth, AUS</option>
+            </select>
+          </label>
+        </div>
+        <br></br>
+        <label>Dietary Restrictions</label>
+        {this.state.dietaryRestrictions.map((restriction, i) => {
+          return <div>
+            <input key={ restriction + i } name={ restriction.label }
+              value={ restriction.label } type="checkbox"
+              onChange={ () => this.handleToggleRestriction(restriction) }
+            />{ restriction.label }
           </div>
-          <div>
-            <label>
-              Gender:
-                Male
-                <input name="gender" value="male" type="radio" onChange={ this.handleUserData } />
-                Female
-                <input name="gender"     value="female"     type="radio"     onChange={ this.handleUserData }   />
-                Neither
-                <input name="gender"
-                  value="neither"
-                  type="radio"
-                  onChange={ this.handleUserData }
-                />
-            </label>
-          </div>
-          <div>
-            <label>
-              Destination:
-              <select name="destination"
-                onChange={ this.handleUserData }>
-                <option></option>
-                <option name="destination"
-                  value="Denver, CO"
-                  onChange={ this.handleUserData }>
-                  Denver, CO
-                </option>
-                <option name="destination"
-                  value="Jacksonville, FL"
-                  onChange={ this.handleUserData }>
-                  Jacksonville, FL
-                </option>
-                <option name="destination"
-                  value="Perth, AUS"
-                  onChange={ this.handleUserData }>
-                  Perth, AUS
-                </option>
-              </select>
-            </label>
-          </div>
-          {this.state.dietaryRestrictions.map((restriction, i) => {
-            return <div>
-                      <input key={ restriction + i }
-                        name={ restriction.label }
-                        id={ restriction.id }
-                        value={ restriction.label }
-                        type="checkbox"
-                        onChange={ () => this.handleToggleRestriction(restriction) }
-                      />{ restriction.label }
-                    </div>
-          })}
-          <button onClick={ this.handleSubmit }>Submit</button>
-        </form>
+        })}
+        <br></br>
+        <button onClick={ this.handleSubmit }>Submit</button>
+      </form>
     )
   }
 }
